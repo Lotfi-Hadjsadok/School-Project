51ea41d3a809cf748d22d49e1b217899444c38c2
Lotfi-Hadjsadok <83740857+Lotfi-Hadjsadok@users.noreply.github.com>
2021-12-01 15:15:51 +0100

    courses faculty api

--- inc/CourseApis.php
+++ inc/CourseApis.php
@@ -12,38 +12,10 @@ class CourseApis
             'methods' => 'GET',
             'callback' => array(CourseApis::class, 'search_courses')
         ));
-        register_rest_route('university/v1', 'search', array(
+        register_rest_route('university/v1', 'courses_faculty', array(
             'methods' => 'GET',
-            'callback' => array(CourseApis::class, 'search_results')
+            'callback' => array(CourseApis::class, 'search_courses_faculties')
         ));
-    }
-    function search_results($data)
-    {
-        $courses = new WP_Query(array(
-            'post_type' => ['course'],
-            'post_status' => 'publish',
-            'posts_per_page' => 10,
-            'paged' => $data['page'],
-            's' => sanitize_text_field($data['query']),
-            'p' => sanitize_text_field($data['id'])
-        ));
-        $data = [];
-        while ($courses->have_posts()) {
-            $courses->the_post();
-
-            array_push($data, array(
-                'id' => get_the_ID(),
-                'title' => get_the_title(),
-                'module' => get_the_terms(0, 'module')[0],
-                'type' => get_the_terms(0, 'type')[0],
-                'faculty' => get_the_terms(0, 'faculty')[0],
-                'description' => get_the_content(),
-                'post_link' => get_the_permalink(),
-                'status' => get_post_status(),
-                'youtube_link' => get_field('course_video_url'),
-                'course_rar' => get_field('course_rar')
-            ));
-        }
     }
     function get_the_course_term($taxonomy)
     {
@@ -55,10 +27,49 @@ class CourseApis
         $courses = new WP_Query(array(
             'post_type' => ['course'],
             'post_status' => 'publish',
-            'posts_per_page' => 10,
+            'posts_per_page' => $data['page_limit'],
             'paged' => $data['page'],
             's' => sanitize_text_field($data['query']),
-            'p' => sanitize_text_field($data['id'])
+            'p' => sanitize_text_field($data['id']),
+        ));
+
+        $results = [];
+        while ($courses->have_posts()) {
+            $courses->the_post();
+            array_push($results, array(
+                'id' => get_the_ID(),
+                'title' => get_the_title(),
+                'module' => $course->get_the_course_term('module'),
+                'type' => $course->get_the_course_term('type'),
+                'faculty' => $course->get_the_course_term('faculty'),
+                'description' => get_the_content(),
+                'post_link' => get_the_permalink(),
+                'status' => get_post_status(),
+                'youtube_link' => get_field('course_video_url'),
+                'course_rar' => get_field('course_rar')
+            ));
+        }
+        return $results;
+    }
+
+    // PAR FACULTY
+    function search_courses_faculties($data)
+    {
+        $course = new CourseApis();
+        $courses = new WP_Query(array(
+            'post_type' => ['course'],
+            'post_status' => 'publish',
+            'posts_per_page' => $data['page_limit'],
+            'paged' => $data['page'],
+            's' => sanitize_text_field($data['query']),
+            'p' => sanitize_text_field($data['id']),
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'faculty',
+                    'field' => 'slug',
+                    'terms' => array($data['faculty'])
+                )
+            )
         ));
 
         $results = [];
